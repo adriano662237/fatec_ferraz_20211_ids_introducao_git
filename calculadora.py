0d71c1d0eb5f942551a0cc898236b5794304ddbc
Adriano Oliveira <adriano.662237@gmail.com>
2021-05-25 08:51:32 -0300

    faz soma, subtração, multiplicação e divisão

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,12 @@
  a = int(input("Digite o primeiro valor"))
  b = int(input("Digite o segundo valor"))
- operacao = input("+: Soma\n-: Subtração")
+ operacao = input("+: Soma\n-: Subtração\n: Multiplicação\n/:Divisão")
  if operação == '+':
      resultado = a + b
-else:
+elif operação == '-'
     resultado = a - b
+elif operação == '*'
+    resultado = a * b
+else:
+    resultado = a // b
  print(resultado)
